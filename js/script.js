d5e1a8489366c0554e351bb7c1ffdebe88975c08
Maria Stachie <mary.stachie@gmail.com>
2022-03-09 16:41:49 +0100

    prova

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 console.log('OK JS')
+
+const database = ['mary@gmail.com', 'rosa@gmail.com', 'sara@gmail.com', 'claudia@gmail.com'];
+const userEmail = document.getElementById('userEmail');
+const button = document.getElementById('buttonVerify');
+
+button.addEventListener('click', function () {
+    for (let i = 0; i < database.length; i++)
+        if (userEmail == database[i]) {
+            console.log('Access allowed')
+            break;
+        }
+        else {
+            console.log('Access denied')
+            break;
+        }
+}
+);
+
+/*
+for (let i = 0; i < database.length; i++) {
+    console.log(database[i])
+} */
